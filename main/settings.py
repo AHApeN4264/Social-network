0a0660954061c4b5b4e718b51b6498084880f03a
AHApeN4264 <kofanandrej3@example.com>
2026-01-16 13:12:13 +0200

    Add_Bot_Bin_Currency_conversion_Security_Stikers_Uploads_photos/videos_Account_recovery_Copy/edit/delete_messages_Scroll_Hour_format_Support_Edit_Color_theme_Chat_Terms_Subscription_Profile

--- main/settings.py
+++ main/settings.py
@@ -100,8 +100,7 @@ CHANNEL_LAYERS = {
 }
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
